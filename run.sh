3a5e27db7a46300b2f883d575c85d5ade7f52055
siyue <siyue.zhang.sg@gmail.com>
2025-04-24 10:42:20 +0800

    train config

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@
 # declare -A MODELS
 # MODELS["Qwen/Qwen2.5-7B-Instruct"]=""
 # MODELS["siyue/Dream_emb"]="/home/siyue/Projects/diffusion_embedder/output/Dream-msmarco-w-instructions/MSMARCO_train_m-Dream_emb_p-mean_b-128_l-304_bidirectional-True_e-1_s-42_w-20_lr-5e-05_lora_r-32/checkpoint-250"
-# MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]=""
+# MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]="/home/siyue/Projects/diffusion_embedder/output/Meta-Llama-3-8B-Instruct-mntp-msmarco-w-instructions/MSMARCO_train_m-Meta-Llama-3-8B-Instruct_p-mean_b-128_l-304_bidirectional-True_e-1_s-42_w-20_lr-5e-05_lora_r-32/checkpoint-250"
 # MODELS["mistralai/Mistral-7B-Instruct-v0.2"]="/home/siyue/Projects/diffusion_embedder/output/Mistral-7B-Instruct-mntp-msmarco-w-instructions/MSMARCO_train_m-Mistral-7B-Instruct-v0.2_p-mean_b-128_l-304_bidirectional-True_e-1_s-42_w-20_lr-5e-05_lora_r-32/checkpoint-250"
 
 # TASKS=("News21InstructionRetrieval" "Core17InstructionRetrieval" "Robust04InstructionRetrieval" )
@@ -25,9 +25,9 @@
 
 #     # Custom output suffix based on model name
 #     if [[ "$MODEL_NAME" == "Dream_emb" ]]; then
-#         SUFFIX="e5"
+#         SUFFIX="msmarco-w-instructions"
 #     else
-#         SUFFIX="mntp-e5"
+#         SUFFIX="mntp-msmarco-w-instructions"
 #     fi
 
 #     for TASK in "${TASKS[@]}"; do
@@ -47,9 +47,9 @@
 # set 4 GPUs
 
 # torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Dream_long.json
-torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Mistral_long.json
-torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/MetaLlama3_long.json
-torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Qwen2_long.json
+# torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Mistral_long.json
+# torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/MetaLlama3_long.json
+# torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Qwen2_long.json
 
 ## test for long document retrieval
 ## set 1 GPU
@@ -57,10 +57,11 @@ torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/d
 # declare -A MODELS
 # MODELS["Qwen/Qwen2.5-7B-Instruct"]="/home/siyue/Projects/diffusion_embedder/output/Qwen2.5-7B-Instruct-mntp-e5/E5_train_m-Qwen2.5-7B-Instruct_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-5e-05_lora_r-16/checkpoint-1000"
 # MODELS["siyue/Dream_emb"]="/home/siyue/Projects/diffusion_embedder/output/Dream-e5/E5_train_m-Dream_emb_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-5e-05_lora_r-16/checkpoint-1000"
-# MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]="/home/siyue/Projects/diffusion_embedder/output/Meta-Llama-3-8B-Instruct-mntp-e5/E5_train_m-Meta-Llama-3-8B-Instruct_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-5e-05_lora_r-16/checkpoint-1000"
-# MODELS["mistralai/Mistral-7B-Instruct-v0.2"]="/home/siyue/Projects/diffusion_embedder/output/Mistral-7B-Instruct-mntp-e5/E5_train_m-Mistral-7B-Instruct-v0.2_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-5e-05_lora_r-16/checkpoint-1000"
+# MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]="/home/siyue/Projects/diffusion_embedder/output/Meta-Llama-3-8B-Instruct-mntp-e5/E5_train_m-Meta-Llama-3-8B-Instruct_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-1000"
+# MODELS["mistralai/Mistral-7B-Instruct-v0.2"]="/home/siyue/Projects/diffusion_embedder/output/Mistral-7B-Instruct-mntp-e5/E5_train_m-Mistral-7B-Instruct-v0.2_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-1000"
 
 # TASKS=("LEMBNarrativeQARetrieval" "LEMBQMSumRetrieval" "LEMBWikimQARetrieval" "LEMBSummScreenFDRetrieval" "LEMBNeedleRetrieval" "LEMBPasskeyRetrieval")
+# TASKS=("LEMBNarrativeQARetrieval")
 
 # for MODEL in "${!MODELS[@]}"; do
 #     PEFT="${MODELS[$MODEL]}"
@@ -91,7 +92,7 @@ torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/d
 
 # torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Dream_theorem.json
 # torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Mistral_theorem.json
-# torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/MetaLlama3_theorem.json
+torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/MetaLlama3_theorem.json
 # torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Qwen2_theorem.json
 
 ## test for reasoning-intensive retrieval
@@ -100,10 +101,11 @@ torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/d
 # declare -A MODELS
 # MODELS["Qwen/Qwen2.5-7B-Instruct"]=""
 # MODELS["siyue/Dream_emb"]=""
-# MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]=""
-# MODELS["mistralai/Mistral-7B-Instruct-v0.2"]=""
+# MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]="/home/siyue/Projects/diffusion_embedder/output/Meta-Llama-3-8B-Instruct-mntp-TheoremAug/E5Mix_train_m-Meta-Llama-3-8B-Instruct_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-5e-05_lora_r-16/checkpoint-581"
+# MODELS["mistralai/Mistral-7B-Instruct-v0.2"]="/home/siyue/Projects/diffusion_embedder/output/Mistral-7B-Instruct-mntp-TheoremAug/E5Mix_train_m-Mistral-7B-Instruct-v0.2_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-5e-05_lora_r-16/checkpoint-581"
 
 # TASKS=("BrightTheoremqaTheorems" "BrightTheoremqaQuestions" "BrightAops" "BrightLeetcode")
+# TASKS=("BrightTheoremqaTheorems" "BrightTheoremqaQuestions")
 
 # for MODEL in "${!MODELS[@]}"; do
 #     PEFT="${MODELS[$MODEL]}"
@@ -111,9 +113,9 @@ torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/d
 
 #     # Custom output suffix based on model name
 #     if [[ "$MODEL_NAME" == "Dream_emb" ]]; then
-#         SUFFIX="e5"
+#         SUFFIX="TheoremAug"
 #     else
-#         SUFFIX="mntp-e5"
+#         SUFFIX="mntp-TheoremAug"
 #     fi
 
 #     for TASK in "${TASKS[@]}"; do
@@ -122,7 +124,12 @@ torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/d
 #             --base_model_name_or_path "$MODEL" \
 #             --peft_model_name_or_path "$PEFT" \
 #             --task_name "$TASK" \
-#             --output_dir "results/LEMB/${TASK}/${MODEL_NAME}-${SUFFIX}" \
-#             --batch_size 8
+#             --output_dir "results/BRIGHT/${TASK}/${MODEL_NAME}-${SUFFIX}" \
+#             --batch_size 16
 #     done
 # done
+
+
+
+
+
