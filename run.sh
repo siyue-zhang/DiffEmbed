d296240041cd9b3d037f6b3a7edaf9f08ad077b6
siyue <siyue.zhang.sg@gmail.com>
2025-04-27 12:54:53 +0800

    revise theoremaug

--- run.sh
+++ run.sh
@@ -48,7 +48,7 @@
 
 # torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Dream_long.json
 # torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Mistral_long.json
-# torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/MetaLlama3_long.json
+torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/MetaLlama3_long.json
 # torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Qwen2_long.json
 
 ## test for long document retrieval
@@ -99,40 +99,40 @@
 ## test for reasoning-intensive retrieval
 # set 4 GPUs
 
-declare -A MODELS
+# declare -A MODELS
 # MODELS["Qwen/Qwen2.5-7B-Instruct"]=""
 # MODELS["siyue/Dream_emb"]="/home/siyue/Projects/diffusion_embedder/output/Dream-TheoremAug/E5Mix_train_m-Dream_emb_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-680"
 # MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]="/home/siyue/Projects/diffusion_embedder/output/Meta-Llama-3-8B-Instruct-mntp-TheoremAug/E5Mix_train_m-Meta-Llama-3-8B-Instruct_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-680"
 # MODELS["mistralai/Mistral-7B-Instruct-v0.2"]="/home/siyue/Projects/diffusion_embedder/output/Mistral-7B-Instruct-mntp-TheoremAug/E5Mix_train_m-Mistral-7B-Instruct-v0.2_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-680"
 
-MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]="/home/siyue/Projects/diffusion_embedder/output/!Meta-Llama-3-8B-Instruct-mntp-TheoremAug/E5Mix_train_m-Meta-Llama-3-8B-Instruct_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-680"
-MODELS["mistralai/Mistral-7B-Instruct-v0.2"]="/home/siyue/Projects/diffusion_embedder/output/!Mistral-7B-Instruct-mntp-TheoremAug/E5Mix_train_m-Mistral-7B-Instruct-v0.2_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-680"
+# MODELS["meta-llama/Meta-Llama-3-8B-Instruct"]="/home/siyue/Projects/diffusion_embedder/output/!Meta-Llama-3-8B-Instruct-mntp-TheoremAug/E5Mix_train_m-Meta-Llama-3-8B-Instruct_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-680"
+# MODELS["mistralai/Mistral-7B-Instruct-v0.2"]="/home/siyue/Projects/diffusion_embedder/output/!Mistral-7B-Instruct-mntp-TheoremAug/E5Mix_train_m-Mistral-7B-Instruct-v0.2_p-mean_b-16_l-4096_bidirectional-True_e-1_s-42_w-100_lr-0.0001_lora_r-16/checkpoint-680"
 
 # TASKS=("BrightTheoremqaTheorems" "BrightTheoremqaQuestions" "BrightAops" "BrightLeetcode")
 # TASKS=("BrightTheoremqaTheorems" "BrightTheoremqaQuestions" )
-TASKS=("BrightAops" "BrightLeetcode")
+# TASKS=("BrightAops" "BrightLeetcode")
 
-for MODEL in "${!MODELS[@]}"; do
-    PEFT="${MODELS[$MODEL]}"
-    MODEL_NAME=$(basename "$MODEL")
+# for MODEL in "${!MODELS[@]}"; do
+#     PEFT="${MODELS[$MODEL]}"
+#     MODEL_NAME=$(basename "$MODEL")
 
-    # Custom output suffix based on model name
-    if [[ "$MODEL_NAME" == "Dream_emb" ]]; then
-        SUFFIX="TheoremAug"
-    else
-        SUFFIX="mntp-TheoremAug"
-    fi
+#     # Custom output suffix based on model name
+#     if [[ "$MODEL_NAME" == "Dream_emb" ]]; then
+#         SUFFIX="TheoremAug"
+#     else
+#         SUFFIX="mntp-TheoremAug"
+#     fi
 
-    for TASK in "${TASKS[@]}"; do
-        echo "Running $TASK with $MODEL_NAME..."
-        python experiments/mteb_eval_custom.py \
-            --base_model_name_or_path "$MODEL" \
-            --peft_model_name_or_path "$PEFT" \
-            --task_name "$TASK" \
-            --output_dir "results/BRIGHT/${TASK}/${MODEL_NAME}-${SUFFIX}" \
-            --batch_size 16
-    done
-done
+#     for TASK in "${TASKS[@]}"; do
+#         echo "Running $TASK with $MODEL_NAME..."
+#         python experiments/mteb_eval_custom.py \
+#             --base_model_name_or_path "$MODEL" \
+#             --peft_model_name_or_path "$PEFT" \
+#             --task_name "$TASK" \
+#             --output_dir "results/BRIGHT/${TASK}/${MODEL_NAME}-${SUFFIX}" \
+#             --batch_size 16
+#     done
+# done
 
 
 # torchrun --nproc_per_node=4 experiments/run_supervised.py /home/siyue/Projects/diffusion_embedder/train_configs/supervised/Mistral_theorem.json
