7a974e5f143ed6b66f2f8fa0ba597ea787b26e2c
siyue <siyue.zhang.sg@gmail.com>
2025-05-18 13:46:37 +0800

    revise

--- assets/plot_steps.py
+++ assets/plot_steps.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
 # Data points
-dream_x = [3200, 4800, 6400, 9600, 10880]
-dream_y = [37.7, 42.2, 43.8, 48.3]  # Replace with actual Dream data if available
-qwen_x = [3200, 6400, 9600, 10880]
-qwen_y = [36.4, 35.9, 38, 40.2]
+dream_x = [3200, 4800, 6400, 8000, 9600, 10880]
+dream_y = [32.2, 39.1, 45.8, 44.8, 47.4, 48.3]  # Replace with actual Dream data if available
+qwen_x = [3200, 4800, 6400, 8000, 9600, 10880]
+qwen_y = [36.4, 30.5, 35.9, 35.3, 38, 40.2]
 
 # Formatter function for x-axis
 def format_x_k(x, pos):
@@ -21,9 +21,9 @@ plt.plot(qwen_x, qwen_y, label='Qwen2.5', color='blue', marker='o', markersize=1
 # Apply formatter to x-axis
 plt.gca().xaxis.set_major_formatter(FuncFormatter(format_x_k))
 plt.xticks([2000, 4000, 6000, 8000, 10000, 12000], fontsize=24)
-plt.yticks([35, 40, 45, 50], fontsize=24)
+plt.yticks([30, 35, 40, 45, 50], fontsize=24)
 
-plt.ylim([33,50])
+plt.ylim([28,52])
 # Axis labels
 # plt.xlabel('Number of training samples', fontsize=20, labelpad=15)
 plt.ylabel('TheoQ. nDCG@10(%)', fontsize=24, labelpad=20)
